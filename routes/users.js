af412fe5206bbae13744f4307cce93f2e2f1259f
HEYxJENN <95577576+HEYxJENN@users.noreply.github.com>
2023-01-05 15:20:24 +0400

    fix:mb

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi, Segments } = require('celebrate');
 const {
   getUser,
   createUser,
@@ -12,9 +13,42 @@ const {
 router.get('/users/me', getMe);
 router.get('/users', getUsers);
 router.get('/users/:userId', getUser);
-// router.post('/users', createUser);
-router.patch('/users/me', updateUser);
-router.patch('/users/me/avatar', updateUseravatar);
+router.patch(
+  '/users/me',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().min(2).max(30).default('Жак-Ив-Кусто'),
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(1),
+      about: Joi.string().min(2).max(30).default('Исследователь'),
+      avatar: Joi.string()
+        .min(2)
+        .max(30)
+        .default(
+          'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'
+        ),
+    }),
+  }),
+  updateUser
+);
+router.patch(
+  '/users/me/avatar',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().min(2).max(30).default('Жак-Ив-Кусто'),
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(1),
+      about: Joi.string().min(2).max(30).default('Исследователь'),
+      avatar: Joi.string()
+        .min(2)
+        .max(30)
+        .default(
+          'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'
+        ),
+    }),
+  }),
+  updateUseravatar
+);
 router.post('/signup', createUser);
 router.post('/signin', login);
 
